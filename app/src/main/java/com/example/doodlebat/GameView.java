c5b935836d0d4f7da792f99c6c7390192c0b8e56
fmathisc <mathischerbonnier1207@gmail.com>
2025-03-21 12:03:43 +0100

    merge modif

--- app/src/main/java/com/example/doodlebat/GameView.java
+++ app/src/main/java/com/example/doodlebat/GameView.java
@@ -47,10 +47,6 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback, Sen
 
     private Obstacle lastObstacle1 = null;
     private Obstacle lastObstacle2 = null;
-    private static final int FRAME_DELAY = 10;
-    private Bitmap background, scaledBackground;
-    private float backgroundX = 0;
-    private int backgroundSpeed = 5;
     private long lastScoreUpdateTime = System.currentTimeMillis();
     private boolean gameOver = false;
     private GameOverListener gameOverListener;
